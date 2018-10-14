f433ac13f4e953367beb4da99db739c420a7148b
AbhiRuchiK <33430806+AbhiRuchiK@users.noreply.github.com>
2018-10-14 22:59:45 +0530

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,4 +1,4 @@
-import numpy as n #for vectorized calculation
+import numpy as n 
 #a= [int(x) for x in input("Enter values : ").split("")]
 a=list(map(int,input().split()))
 
@@ -18,8 +18,7 @@ while i != (n.size(a)):
             break
     i=i+1
     
-print("sorted values is: {}".format(a)) #its printing the values
-
+print("sorted values is: {}".format(a)) 
 
 
 
